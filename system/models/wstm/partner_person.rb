3032eb4a4a954ade610cfb44fb234ba5f420e196
kfl62 <kfl62g@gmail.com>
2012-08-18 18:33:21 +0300

    model: Wstm::PartnerPerson specific model for Client PF

---
+++ system/models/wstm/partner_person.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+module Wstm
+  class PartnerPerson < Trst::Person
+    has_one :address,  class_name:  'Trst::Address', as: :person_address
+
+    class << self
+      def default_sort
+        asc(:name_last,:name_frst)
+      end
+    end # Class methods
+    # @todo
+    def view_filter
+      [id, name, id_pn]
+    end
+  end # PartnerPerson
+end # Wstm
