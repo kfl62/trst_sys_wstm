868e34c55389fa8092329e949c36e332127c0ab9
kfl62 <kfl62g@gmail.com>
2012-08-18 22:25:25 +0300

    model: Wstm::PartnerPerson accept nested attributes for address

--- system/models/wstm/partner_person.rb
+++ system/models/wstm/partner_person.rb
@@ -2,6 +2,7 @@
 module Wstm
   class PartnerPerson < Trst::Person
     has_one :address,  class_name:  'Trst::Address', as: :person_address
+    accepts_nested_attributes_for :address
 
     class << self
       def default_sort
