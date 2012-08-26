4c4a758602e907342d60ddde74f59655874736f0
kfl62 <kfl62g@gmail.com>
2012-08-26 20:31:27 +0300

    model: Wstm::PartnerPerson.auto_search(params) new method

--- system/models/wstm/partner_person.rb
+++ system/models/wstm/partner_person.rb
@@ -5,8 +5,17 @@ module Wstm
     accepts_nested_attributes_for :address
 
     class << self
+      # @todo
       def default_sort
         asc(:name_last,:name_frst)
+      end
+      # @todo
+      def auto_search(params)
+        default_sort.only(:id,:id_pn,:name_last,:name_frst)
+        .or(id_pn: /\A#{params[:q]}/)
+        .or(name_last: /\A#{params[:q]}/i)
+        .or(name_frst: /\A#{params[:q]}/i)
+        .each_with_object([]){|pf,a| a << {id: pf.id.to_s,text: "#{pf.id_pn.ljust(18)} #{pf.name}"}}
       end
     end # Class methods
     # @todo
