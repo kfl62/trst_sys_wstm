60a86b5879712ac667750cc8fb28fcf1af425211
kfl62 <kfl62g@gmail.com>
2012-09-24 23:34:34 +0300

    models: Wstm::FreightsOut, typo, indexes, scopes, callbacks, #handle_stock

--- system/models/wstm/freight_out.rb
+++ system/models/wstm/freight_out.rb
@@ -11,16 +11,21 @@ module Wstm
     field :id_intern,   type: Boolean,   default: false
     field :um,          type: String,    default: "kg"
     field :pu,          type: Float,     default: 0.00
-    field :pu_invoice,  type: Float,     default: 0.00
     field :qu,          type: Float,     default: 0.00
     field :val,         type: Float,     default: 0.00
+    field :pu_invoice,  type: Float,     default: 0.00
     field :val_invoice, type: Float,     default: 0.00
 
     belongs_to  :freight,  class_name: 'Wstm::Freight',     inverse_of: :outs
-    belongs_to  :doc_dln,  class_name: 'Wstm::DeliveyNote', inverse_of: :freights
+    belongs_to  :doc_dln,  class_name: 'Wstm::DeliveryNote',inverse_of: :freights
     belongs_to  :doc_cas,  class_name: 'Wstm::Cassation',   inverse_of: :freights
 
+    after_save    :'handle_stock(false)'
+    after_destroy :'handle_stock(true)'
+
     index({ freight_id: 1, id_date: 1 })
+    index({ doc_dln_id: 1})
+    index({ doc_cas_id: 1})
 
     class << self
       # @todo
@@ -66,6 +71,82 @@ module Wstm
     # @todo
     def doc
       doc_dln || doc_cas
+    end
+
+    private
+    # @todo
+    def handle_stock(add_delete)
+      today = Date.today; retro = id_date.month == today.month
+      stock_to_handle = retro ? [unit.stock_now] : [unit.stock_monthly(id_date.year,id_date.month), unit.stock_now]
+      if add_delete
+        stock_to_handle.each do |stck|
+          f = stck.freights.find_or_create_by(id_stats: id_stats, pu: pu)
+          f.freight_id = freight_id
+          f.qu += qu
+          f.val = (f.pu * f. qu).round(2)
+          f.save
+        end
+      else
+        stock_to_handle.each_with_index do |stck,i|
+          out = qu
+          lpu = freight.ins.last.pu == 0 ? freight.pu : freight.ins.last.pu
+          fs  = stck.freights.where(id_stats: id_stats)
+          if fs.count == 1
+            f     = fs.first
+            f.qu -= out
+            f.val = (f.pu * f.qu).round(2)
+            f.save
+            pu = f.pu
+            val= (pu * qu).round(2)
+          else
+            fspus = fs.where(:qu.ne => 0).asc(:pu).map(&:pu)
+            fspus.delete(lpu).nil? ? fspus : fspus.push(lpu)
+            fspus.delete(0).nil?   ? fspus : fspus.push(0)  if unit.main?
+            fspus.each do |fspu|
+              f = fs.find_by(pu: fspu)
+              if out > f.qu
+                if i == 0
+                  self.class.new(
+                    id_date:    id_date,
+                    id_stats:   f.id_stats,
+                    id_intern:  id_intern,
+                    um:         f.um,
+                    pu:         f.pu,
+                    qu:         f.qu,
+                    val:        (f.pu * f.qu).round(2),
+                    freight_id: f.freight.id,
+                    doc_dln_id: (doc_dln_id if doc_dln_id),
+                    doc_cas_id: (doc_cas_id if doc_cas_id)
+                  ).upsert
+                end # if stock_to_handle.first
+                out -= f.qu unless out == 0
+                f.qu = 0
+                f.val= 0
+                f.save
+              else
+                if i == 0
+                  self.class.new(
+                    id_date:    id_date,
+                    id_stats:   f.id_stats,
+                    id_intern:  id_intern,
+                    um:         f.um,
+                    pu:         f.pu,
+                    qu:         out,
+                    val:        (f.pu * out).round(2),
+                    freight_id: f.freight.id,
+                    doc_dln_id: (doc_dln_id if doc_dln_id),
+                    doc_cas_id: (doc_cas_id if doc_cas_id)
+                  ).upsert unless out == 0
+                end # if stock_to_handle.first
+                f.qu -= out
+                f.val = (f.pu * f.qu).round(2)
+                f.save
+              end # if out > f.qu
+            end # each fspu
+            self.delete if i == stock_to_handle.length - 1
+          end # if fs.count == 1
+        end # each stck
+      end # if add_delete
     end
 
   end # FreightOut
