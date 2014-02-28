437ecf77f784572eae77f80bda6b5d6b553d5154
kfl62 <kfl62g@gmail.com>
2014-02-28 12:59:48 +0200

    views: reports pdf templates, ruby 2 hash syntax, updated prawn caused modifications

--- system/views/system/wstm/expenditure/pdf.rb
+++ system/views/system/wstm/expenditure/pdf.rb
@@ -3,7 +3,7 @@
 require 'prawn/measurement_extensions'
 
 def firm
-  Wstm::PartnerFirm.find_by(:firm => true)
+  Wstm::PartnerFirm.find_by(firm: true)
 end
 def address
   firm.addresses.first
@@ -87,103 +87,103 @@ def box_content(pdf)
   pdf.font_size 8 do
     pdf.text "#{firm.name[2]}"
     pdf.move_up 9
-    pdf.text "Punct de lucru: #{@object.unit.name[1]}", :align => :right
+    pdf.text "Punct de lucru: #{@object.unit.name[1]}", align: :right
     pdf.text "Nr.înreg.R.C. : #{firm.identities['chambcom']}"
     pdf.move_up 9
-    pdf.text "Gestionar: #{@object.signed_by.name}", :align => :right
+    pdf.text "Gestionar: #{@object.signed_by.name}", align: :right
     pdf.text "Cod fiscal (C.U.I) : #{firm.identities['fiscal']}"
     pdf.text "Str.#{address.street} nr.#{address.nr},bl.#{address.bl},sc.#{address.sc},ap.#{address.ap},"
     pdf.text 'Cluj-Napoca, judeţul Cluj'
   end
   pdf.move_down 5.mm
   pdf.text 'ADEVERINŢĂ DE PRIMIRE ŞI PLATĂ',
-    :align => :center, :size => 12, :style => :bold
+    align: :center, size: 12, style: :bold
   pdf.move_down 5.mm
   if @object.name == 'EMPTY'
     pdf.text "Nr. #{'_'*5} din #{'_'*15}",
-      :align => :center, :size => 10, :style => :bold
+      align: :center, size: 10, style: :bold
   else
     pdf.text "Nr. #{@object.name} din #{@object.id_date.strftime('%Y-%m-%d')}",
-      :align => :center, :size => 10, :style => :bold
+      align: :center, size: 10, style: :bold
   end
   pdf.move_down 5.mm
   pdf.font_size 9 do
     if @object.name == 'EMPTY'
-      pdf.text I18n.t('wstm.intro.pdf.desk_expenditure.text_empty_01',client_hash),:leading => 2, :inline_format => true
+      pdf.text I18n.t('wstm.intro.pdf.desk_expenditure.text_empty_01',client_hash),leading: 2, inline_format: true
     else
-      pdf.text I18n.t('wstm.intro.pdf.desk_expenditure.text_01',client_hash),:leading => 2, :inline_format => true
+      pdf.text I18n.t('wstm.intro.pdf.desk_expenditure.text_01',client_hash),leading: 2, inline_format: true
     end
     pdf.move_down 10.mm
     pdf.table(table_freight_data) do
-      style(row(0..7), :padding => [1,3])
-      style(row(0), :background_color => 'dddddd', :align => :center)
-      style(row(1..5).column(0), :align => :center)
-      style(row(1..5).column(2), :align => :center)
-      style(row(1..5).column(3..8), :align => :right)
-      column(0).style(:width => 6.mm)
-      column(1).style(:width => 18.mm)
-      column(2).style(:width => 8.mm)
-      column(3).style(:width => 13.mm)
-      column(4).style(:width => 15.mm)
-      column(5).style(:width => 18.mm)
-      column(6).style(:width => 12.mm)
-      column(7).style(:width => 15.mm)
-      column(8).style(:width => 18.mm)
+      style(row(0..7), padding: [1,3])
+      style(row(0), background_color: 'dddddd', align: :center)
+      style(row(1..5).column(0), align: :center)
+      style(row(1..5).column(2), align: :center)
+      style(row(1..5).column(3..8), align: :right)
+      column(0).style(width: 6.mm)
+      column(1).style(width: 18.mm)
+      column(2).style(width: 8.mm)
+      column(3).style(width: 13.mm)
+      column(4).style(width: 15.mm)
+      column(5).style(width: 18.mm)
+      column(6).style(width: 12.mm)
+      column(7).style(width: 15.mm)
+      column(8).style(width: 18.mm)
       row(6).column(0).borders = [:left,:top,:bottom]
       row(6).column(1..4).borders = [:top,:bottom]
-      style(row(6), :background_color => 'dddddd', :align => :center)
+      style(row(6), background_color: 'dddddd', align: :center)
     end
     pdf.move_down 10.mm
     if @object.name == 'EMPTY'
-      pdf.text I18n.t('wstm.intro.pdf.desk_expenditure.text_empty_02',client_hash).gsub('#',"#{Prawn::Text::NBSP}"),:leading => 2, :inline_format => true
+      pdf.text I18n.t('wstm.intro.pdf.desk_expenditure.text_empty_02',client_hash).gsub('#',"#{Prawn::Text::NBSP}"),leading: 2, inline_format: true
     else
-      pdf.text I18n.t('wstm.intro.pdf.desk_expenditure.text_02',client_hash).gsub('#',"#{Prawn::Text::NBSP}"),:leading => 2, :inline_format => true
+      pdf.text I18n.t('wstm.intro.pdf.desk_expenditure.text_02',client_hash).gsub('#',"#{Prawn::Text::NBSP}"),leading: 2, inline_format: true
     end
     pdf.draw_text('Semnătura (amprenta) ________________', :at  => [50.mm,pdf.y - 25])
     pdf.move_down 5.mm
     if @object.name == 'EMPTY'
       pdf.table(table_recap_data) do
-        cells.style(:borders => [], :padding => [1,3])
-        column(1).style(:align => :right,:width => 25.mm)
+        cells.style(borders: [], padding: [1,3])
+        column(1).style(align: :right,width: 25.mm)
       end
     else
       pdf.table(table_recap_data) do
-        cells.style(:borders => [], :padding => [1,3])
-        column(1).style(:align => :right,:width => 25.mm,:font_style => :bold)
+        cells.style(borders: [], padding: [1,3])
+        column(1).style(align: :right,width: 25.mm,font_style: :bold)
       end
     end
     pdf.move_down 10.mm
     pdf.table(table_sign_data) do
-      cells.style(:borders => [], :padding => [1,3], :align => :center)
-      column(0..1).style(:width => 60.mm)
+      cells.style(borders: [], padding: [1,3], align: :center)
+      column(0..1).style(width: 60.mm)
     end
   end
 end
 
 
 pdf = Prawn::Document.new(
-  :page_size => 'A4',
-  :page_layout => :landscape,
-  :margin => [0.mm],
-  :info => {
-    :Title => "Adeverinţă_Primire_Plata",
-    :Author => "kfl62",
-    :Subject => "Formular \"Adeverinţă de Primire şi Plată\"",
-    :Keywords => "#{firm.name[1].titleize} Adeverinţă Primire Plată",
-    :Creator => "http://#{firm.name[0].downcase}.trst.ro (using Sinatra, Prawn)",
-    :CreationDate => Time.now
+  page_size: 'A4',
+  page_layout: :landscape,
+  margin: [0.mm],
+  info: {
+    Title: "Adeverinţă_Primire_Plata",
+    Author: "kfl62",
+    Subject: "Formular \"Adeverinţă de Primire şi Plată\"",
+    Keywords: "#{firm.name[1].titleize} Adeverinţă Primire Plată",
+    Creator: "http://#{firm.name[0].downcase}.trst.ro (using Sinatra, Prawn)",
+    CreationDate: Time.now
   }
 )
 pdf.font_families.update(
-  'Verdana' => {:bold => 'public/stylesheets/fonts/verdanab.ttf',
-                :italic => 'public/stylesheets/fonts/verdanai.ttf',
-                :bold_italic => 'public/stylesheets/fonts/verdanaz.ttf',
-                :normal => 'public/stylesheets/fonts/verdana.ttf'})
+  'Verdana' => {bold: 'public/stylesheets/fonts/verdanab.ttf',
+                italic: 'public/stylesheets/fonts/verdanai.ttf',
+                bold_italic: 'public/stylesheets/fonts/verdanaz.ttf',
+                normal: 'public/stylesheets/fonts/verdana.ttf'})
 pdf.font 'Verdana'
-pdf.bounding_box([15.mm,200.mm],:width  => 123.mm, :height => 190.mm) do
+pdf.bounding_box([15.mm,200.mm],:width  => 123.mm, height: 190.mm) do
   box_content(pdf)
 end
-pdf.bounding_box([163.mm,200.mm],:width  => 123.mm, :height => 190.mm) do
+pdf.bounding_box([163.mm,200.mm],:width  => 123.mm, height: 190.mm) do
   box_content(pdf)
 end
 pdf.stroke_line(148.mm,5.mm,148.mm,10.mm)
