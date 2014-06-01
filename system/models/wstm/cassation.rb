533e1231fea391e7664390d307cd222d3cc21b46
kfl62 <kfl62g@gmail.com>
2014-06-01 09:05:21 +0300

    models: `Wstm::Cassation` cleanup, index

--- system/models/wstm/cassation.rb
+++ system/models/wstm/cassation.rb
@@ -14,11 +14,12 @@ module Wstm
 
     alias :file_name :name
 
-    has_many   :freights,   class_name: "Wstm::FreightOut",      inverse_of: :doc_cas, dependent: :destroy
-    belongs_to :unit,       class_name: "Wstm::PartnerFirm::Unit", inverse_of: :csss
-    belongs_to :signed_by,  class_name: "Wstm::User",            inverse_of: :csss
+    has_many   :freights,   class_name: "Wstm::FreightOut",       inverse_of: :doc_cas, dependent: :destroy
+    belongs_to :unit,       class_name: "Wstm::PartnerFirm::Unit",inverse_of: :csss, index: true
+    belongs_to :signed_by,  class_name: "Wstm::User",             inverse_of: :csss
 
     index({ unit_id: 1, id_date: 1 })
+
     scope :by_unit_id, ->(unit_id) {where(unit_id: unit_id)}
 
     accepts_nested_attributes_for :freights,
@@ -27,7 +28,8 @@ module Wstm
     class << self
       # @todo
       def pos(s)
-        where(unit_id: Wstm::PartnerFirm.pos(s).id)
+        uid = Clns::PartnerFirm.pos(s).id
+        by_unit_id(uid)
       end
       # @todo
       def nonin(nin = true)
