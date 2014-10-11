b23de6a1a050d0bce6bcd2e1c58fe77133eff978
kfl62 <kfl62g@gmail.com>
2014-10-11 07:58:13 +0300

    assets: js, `Wstm.desk.freight` cleanup, r_path & #31

--- public/javascripts/wstm/desk_freight.js
+++ public/javascripts/wstm/desk_freight.js
@@ -9,7 +9,7 @@
               $select = $(this);
               $select.on('change', function() {
                 var $params, $url;
-                $params = jQuery.param($('.param').serializeArray());
+                $params = jQuery.param($('[data-mark~=param]').serializeArray());
                 $url = "sys/wstm/freight/query?" + $params;
                 Trst.desk.init($url);
               });
@@ -21,7 +21,7 @@
               $select = $(this);
               $select.on('change', function() {
                 var $params, $url;
-                $params = jQuery.param($('.param').serializeArray());
+                $params = jQuery.param($('[data-mark~=param]').serializeArray());
                 $url = "sys/wstm/freight/query?" + $params;
                 Trst.desk.init($url);
               });
@@ -32,7 +32,7 @@
                 $link = $(this);
                 $link.on('click', function() {
                   var $params, $url;
-                  $params = jQuery.param($('.param').serializeArray());
+                  $params = jQuery.param($('[data-mark~=param]').serializeArray());
                   $url = "sys/wstm/freight/query?" + $params;
                   $url += $link.hasClass('uid') ? "&uid=" + ($link.attr('id')) : "&fid=" + ($link.attr('id'));
                   Trst.desk.init($url);
@@ -40,14 +40,27 @@
               });
             }
           },
+          buttons: function(btns) {
+            btns.each(function() {
+              var $bd, $button, _ref;
+              $button = $(this);
+              $bd = $button.data();
+              if (Trst.desk.hdo.dialog === 'filter') {
+                if ((_ref = $bd.action) === 'create' || _ref === 'show' || _ref === 'edit' || _ref === 'delete') {
+                  return $bd.r_path = 'sys/wstm/freight/filter';
+                }
+              }
+            });
+          },
           init: function() {
             var _ref;
+            this.buttons($('button'));
             if ((_ref = Trst.desk.hdo.dialog) === 'query' || _ref === 'query_value') {
               if ($('#xhr_info').hasClass('noFreight')) {
-                Wstm.desk.freight.handleNoFreight();
+                this.handleNoFreight();
               }
               if ($('#xhr_info').hasClass('hasFreight')) {
-                Wstm.desk.freight.handleFreight();
+                this.handleFreight();
               }
             }
             return $log('Wstm.desk.freight.init() OK...');
